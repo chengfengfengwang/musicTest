31064676729e01e963683c9c77b3c0928a97dc84
wangchengfeng <602120734@qq.com>
2019-06-04 10:14:12 +0800

    随机给一个数，判断是否符合规则

--- src/modules/test/test.vue
+++ src/modules/test/test.vue
@@ -3,42 +3,82 @@
     <div>hello test</div>
     <audio controls src="http://image.yinji.immusician.com/cover/game_material155704803581981.mp3"></audio>
     <button @click="start">开始</button>
-    <div>
-      <span class="beat" v-for="n in 10">正</span>
-    </div>
-    <div class="box box1"></div>
-    <div class="box box2"></div>
-    <div class="box box3"></div>
+    <div class="beat" v-for="n in 20"></div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      time: ""
+      time: "",
+      beats: [],
+      speed: 60,
+      clickTime: "",
+      interval: "",
+      index: 0,
+      error:200
     };
   },
   mounted() {
-    $(".box").animate(
-      {
-        bottom: "350",
-      },
-      10000,
-      function() {
-        // Animation complete.
-      }
-    );
-    document.body.addEventListener("click", e => {
-      console.log(this.time);
-      this.appendT();
-    });
+    // $(".beat").animate(
+    //   {
+    //     bottom: "350",
+    //   },
+    //   10000,
+    //   function() {
+    //     // Animation complete.
+    //   }
+    // );
+    this.interval = 60000 / this.speed;
+    this.bindClick();
   },
   methods: {
-    start() {
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    bindClick() {
+      document.body.addEventListener("click", e => {
+        this.clickTime = this.time;
+        const remainder = this.clickTime % this.interval;
+        const midPoint = this.interval / 2;
+        const errorPoint = this.error / 2; 
+        if((remainder>midPoint)){
+          if(Math.abs(remainder - this.interval) > errorPoint){
+            console.log('false')
+          }else{
+            console.log('right')
+          }
+        }else{
+          if(remainder > errorPoint){
+            console.log('false')
+          }else{
+            console.log('right')
+          }
+        }
+        console.log(this.index, this.time);
+      });
+    },
+    upBeats() {
+      var beats = document.querySelectorAll(".beat");
+      this.beats = beats;
+      beats.forEach((e, i) => {
+        e.style.left = Math.random() * 300 + "px";
+        setTimeout(() => {
+          e.classList.add("beat_up");
+        }, 1000 * i);
+      });
+    },
+    countTime() {
       this.time = 0;
       setInterval(() => {
         this.time += 10;
+        //当前处于第几拍的范围内，从0开始
+        this.index = Math.floor(this.time / this.interval);
       }, 10);
+    },
+    start() {
+      this.countTime();
+      this.upBeats();
     },
     appendT() {
       var span = document.createElement("span");
@@ -58,22 +98,38 @@ body {
   height: 100vh;
   position: relative;
 }
-.box {
+.beat {
   width: 30px;
   height: 30px;
   border-radius: 50%;
   background-color: red;
   position: absolute;
-  bottom: 0;
+  bottom: -30px;
 }
-.box1{
+.beat1 {
   left: 50px;
 }
-.box2{
+.beat2 {
   left: 250px;
 }
-.box3{
+.beat3 {
   left: 130px;
+}
+.beat_up {
+  animation: up 5s linear forwards;
+}
+@keyframes up {
+  0% {
+    bottom: 0;
+  }
+  99% {
+    opacity: 1;
+  }
+  100% {
+    bottom: 850px;
+    opacity: 0;
+    display: none;
+  }
 }
 </style>
 
